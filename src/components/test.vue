603a8540c4d0879a683e73cd2c9953368f82eb2a
小8 <p25791313@gmail.com>
2019-08-16 16:23:01 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="test">
+    <h1>{{title}}</h1>
+    
+    </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+    return{
+        title:"hi"
+    }
+    }
+}
+</script>
+<style>
+.test{
+color:red
+}
+</style>
